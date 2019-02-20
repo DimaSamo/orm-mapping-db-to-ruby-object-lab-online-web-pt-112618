9a201c9ec6788756671c6ec0ea4a3ceee6a77dba
Dmitri Samotessov <dimasamo96@gmail.com>
2019-02-20 05:22:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,7 +48,7 @@ class Student
 
   def self.all_students_in_grade_9
     self.all.map do |student|
-      student if student.grade == 9
+      student if student.grade == "9"
       binding.pry
     end.compact
   end
